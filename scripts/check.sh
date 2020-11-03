a45239a9ef8c9f24e193a9ba506516f237267a2a
Thomas Deutsch <thomas@tuxpeople.org>
2020-11-04 00:24:50 +0100

    wait for cluster to be finished and typo fixed

--- scripts/check.sh
+++ scripts/check.sh
@@ -5,11 +5,17 @@ while [ ! -f /var/lib/cloud/instance/boot-finished ]; do
   sleep 1
 done
 
+while ! kubectl get nodes &> /dev/null
+do
+  echo -e "\033[1;36mWaiting for k3s to be ready..."
+  sleep 1
+done
+
 echo "Cluster ready:"
 echo ""
 kubectl get nodes
 echo ""
-echo "Applying test worload"
+echo "Applying test workload"
 kubectl apply -f https://github.com/paulbouwer/hello-kubernetes/raw/master/yaml/hello-kubernetes.yaml
 echo ""
 echo "Delete it like this: kubectl apply -f https://github.com/paulbouwer/hello-kubernetes/raw/master/yaml/hello-kubernetes.yaml"
