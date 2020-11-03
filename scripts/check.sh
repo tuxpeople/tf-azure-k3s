332b3f72addb58e369096f4ffa08de622fcd3f3e
Thomas Deutsch <thomas@tuxpeople.org>
2020-11-04 00:10:44 +0100

    more detailed echo

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while [ ! -f /var/lib/cloud/instance/boot-finished ]; do
-  echo -e "\033[1;36mWaiting for cloud-init..."
+  echo -e "\033[1;36mWaiting for cloud-init to finish..."
   sleep 1
 done
 
